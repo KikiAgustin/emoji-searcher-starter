c2884441b6442d1d0786d926bafc7451403f96fb
kikiAgustin <kikiagustin62@gmail.com>
2022-05-20 06:46:21 +0600

    fetching api from emoji data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function App() {
+  const [emojisData, setEmojisData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      setLoading(true)
+      try {
+        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28')
+        setEmojisData(res)
+        setLoading(false)
+      } catch (error) {
+        console.error(error)
+        setError(true)
+        setLoading(false)
+      }
+    }
+    fetchEmojis()
+  }, [])
+
+  console.log('loading ?', loading)
+  console.log('error ?', error)
+  console.log('emojisData ?', emojisData)
+
   return (
     <div>
       <h1>Hello, world!</h1>
